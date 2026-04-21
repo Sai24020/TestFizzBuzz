e8de5e0edc9df357fd3ed92a159a77b6614d3fb2
Suzan Al Majdalawi <suzan.al-majdalawi@chasacademy.se>
2026-04-21 12:45:47 +0200

    Update AssemblyInfo and add sourcelink.json for versioning and debugging support

--- obj/Debug/net10.0/TestFizzBuzz.AssemblyInfo.cs
+++ obj/Debug/net10.0/TestFizzBuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestFizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a91398a1f8566240c5a373aad3f99fb03c3539cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestFizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestFizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
